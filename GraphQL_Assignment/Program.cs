4c5852eb283b4245a88d0533f1fc85dc3cca1794
Dana Wilson <danalwil@outlook.com>
2021-04-20 09:37:15 -0400

    Add project files.

---
+++ GraphQL_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQL_Assignment.SystemTextJson;
+
+namespace GraphQL_Assignment
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var schema = Schema.For(@"
+                type Query {
+                    hello: String
+                }
+            ");
+
+            var json = await schema.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = new { Hello = "Hellow World!" }
+            });
+        }
+    }
+}
